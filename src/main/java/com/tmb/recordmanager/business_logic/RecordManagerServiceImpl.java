b9b1589a2d7e3a48cb490e71013652bbda3c8e1d
itstmb <tombreier@gmail.com>
2021-11-28 01:08:12 +0200

    * Missing service annotation

--- src/main/java/com/tmb/recordmanager/business_logic/RecordManagerServiceImpl.java
+++ src/main/java/com/tmb/recordmanager/business_logic/RecordManagerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.tmb.recordmanager.business_logic;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RecordManagerServiceImpl implements RecordManagerService {
     @Override
     public ResponseEntity<List<String>> getRecords(String parent) {
