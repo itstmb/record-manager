030f567e8c47c2b1c4ac9d84f4a79314949ff734
itstmb <tombreier@gmail.com>
2021-11-27 12:32:06 +0200

    Initial test implementation.

--- src/test/java/com/tmb/recordmanager/RecordManagerApplicationTests.java
+++ src/test/java/com/tmb/recordmanager/RecordManagerApplicationTests.java
@@ -1,13 +1,29 @@
 package com.tmb.recordmanager;
 
+import com.tmb.recordmanager.rest.RecordManagerController;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {RecordManagerTestConfiguration.class})
 class RecordManagerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    RecordManagerController recordManagerController;
 
+    @Test
+    public void getEmptyRecordsList() {
+        ResponseEntity<Object> response = recordManagerController.getRecords(StringUtils.EMPTY);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        System.out.println("Asserted!");
+        Assertions.assertEquals(ArrayUtils.EMPTY_STRING_ARRAY, response.getBody());
+    }
 }
