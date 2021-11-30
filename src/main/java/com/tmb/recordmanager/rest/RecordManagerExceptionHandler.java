e1e34697d8e80b06e5134e07932953fce1037de0
itstmb <tombreier@gmail.com>
2021-11-30 18:34:08 +0200

    Catch missing servlet parameter exception and display a correct message to the user

--- src/main/java/com/tmb/recordmanager/rest/RecordManagerExceptionHandler.java
+++ src/main/java/com/tmb/recordmanager/rest/RecordManagerExceptionHandler.java
@@ -4,6 +4,7 @@ import com.tmb.recordmanager.rest.exceptions.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,6 +15,11 @@ public class RecordManagerExceptionHandler {
     @ExceptionHandler({ValidationException.class})
     public ResponseEntity<String> handleValidationException(ValidationException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    public ResponseEntity<String> handleMissingException(MissingServletRequestParameterException ex) {
+        return new ResponseEntity<>("Invalid request parameters", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({Exception.class})
